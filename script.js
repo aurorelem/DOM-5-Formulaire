455926d8b107b9d62f7608d92291bd6d67f05a46
aurorelem <aurorelemaire.pro@gmail.com>
2022-12-09 14:46:33 +0100

    fin exercice 2

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 
+let input = document.getElementById("firstname")
+let span = document.getElementById("display-firstname")
+
+input.addEventListener('keyup', () =>{
+    span.textContent = input.value
+})
+
+let age = document.getElementById("age")
+let truth = document.getElementById("a-hard-truth")
+
+age.addEventListener('keyup', () =>{
+   if(age.value<18){
+    truth.style.visibility = "hidden"
+   } 
+   else{
+    truth.style.visibility = "visible"
+   }
+  /* if(age.value >= 18) {
+        truth.setAttribute("style", "visibility: visible")
+   }*/
+})
